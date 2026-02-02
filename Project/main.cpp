a13bd886b586789fe3aeff6f3a6b44cbd82746f8
masuda2024 <k024g0042@g.neec.ac.jp>
2026-02-02 14:15:28 +0900

    //

--- Project/main.cpp
+++ Project/main.cpp
@@ -96,10 +96,10 @@ Transform uvTransformSprite
 	{0.0f,0.0f,0.0f},
 	{0.0f,0.0f,0.0f}
 };
-    //      Oo
-	//   [>[二]
-	//
-	//
+//      Oo
+//   [>[二]
+//
+//
 Transform cameraTransform
 {
 	{1.0f,1.0f,1.0f},
